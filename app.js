421e9290372c6283047cecfd30ff9913565a6ea3
Alexey Ershov <alex4rom@gmail.com>
2016-07-14 08:22:35 +0300

    Add co-body

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 'use strict';
 
+const render = require('./lib/render');
 const koa = require('koa');
 const route = require('koa-route');
+const parse = require('co-body');
 
 const app = koa();
 
 app.use(route.get('/', index));
 
 function* index() {
-  this.body = "<h1>test</h1>";
+  this.body = yield render('index');
 }
 
 app.listen(3000);
