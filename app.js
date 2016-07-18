006f808c9c008bf69651798e0acc884b2c417fe5
Alexey Ershov <alex4rom@gmail.com>
2016-07-18 08:29:39 +0300

    Add template engine

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const parse = require('co-body');
 
 const app = koa();
 
+let db = ['hello'];
+
 app.use(route.get('/', index));
 
 function* index() {
-  this.body = yield render('index');
+  this.body = yield render('index', { posts: db });
 }
 
 app.listen(3000);
