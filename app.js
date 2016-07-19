2b4f7a4a9ceeff036019aabe9b4fc9adf873692b
Alexey Ershov <alex4rom@gmail.com>
2016-07-19 08:00:36 +0300

    Add logger

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const render = require('./lib/render');
 const koa = require('koa');
 const route = require('koa-route');
 const parse = require('co-body');
+const logger = require('koa-logger');
 
 const app = koa();
 
 let db = ['hello'];
 
+const routes = require('./routes.js');
+
+app.use(logger());
 app.use(route.get('/', index));
 
 function* index() {
