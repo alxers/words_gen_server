f87985d86484de2a836b5156c4819620d7e38730
Alexey Ershov <alex4rom@gmail.com>
2016-07-20 08:10:07 +0300

    Add routes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const render = require('./lib/render');
 const koa = require('koa');
 const route = require('koa-route');
-const parse = require('co-body');
 const logger = require('koa-logger');
 
 const app = koa();
@@ -13,7 +11,7 @@ let db = ['hello'];
 const routes = require('./routes.js');
 
 app.use(logger());
-app.use(route.get('/', index));
+app.use(route.get('/', routes.index));
 
 function* index() {
   this.body = yield render('index', { posts: db });
---
+++ routes.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const parse = require('co-body');
+const render = require('./lib/render');
