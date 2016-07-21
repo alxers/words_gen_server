d3362cdcee061d7b0f01c46eda0a8ec05dc22ad3
Alexey Ershov <alex4rom@gmail.com>
2016-07-21 08:26:22 +0300

    Add routes

--- app.js
+++ app.js
@@ -13,8 +13,4 @@ const routes = require('./routes.js');
 app.use(logger());
 app.use(route.get('/', routes.index));
 
-function* index() {
-  this.body = yield render('index', { posts: db });
-}
-
 app.listen(3000);
--- routes.js
+++ routes.js
@@ -2,3 +2,15 @@
 
 const parse = require('co-body');
 const render = require('./lib/render');
+
+const monk = require('monk');
+const wrap = require('co-monk');
+const db = monk('localhost/wordsGen');
+
+const words = wrap(db.get('words'));
+
+// Route defenitions
+
+module.exports.index = function* index() {
+  this.body = yield render('index', { posts: db });
+}
