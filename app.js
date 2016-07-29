ba98eedd55f5641b94cc3b51a09ecc48791b9136
Alexey Ershov <alex4rom@gmail.com>
2016-07-29 08:23:14 +0300

    Add routes

--- app.js
+++ app.js
@@ -10,5 +10,10 @@ const routes = require('./routes.js');
 
 app.use(logger());
 app.use(route.get('/', routes.list));
+app.use(route.get('/word/new', routes.add));
+app.use(route.post('/word', routes.create));
+app.use(route.post('/word/:id', routes.update));
+app.use(route.get('/word/:id/edit', routes.edit));
+app.use(route.get('word/:id/delete', routes.remove));
 
 app.listen(3000);
